debfa8e74e371f8a31ad3b0ef3ee33be600497c1
Mahadi Hasan <120239568+mahadi-493@users.noreply.github.com>
2025-08-22 21:55:43 +0600

    Update index.js

--- index.js
+++ index.js
@@ -337,4 +337,73 @@
 // nafiz.eat();
 
 //super 
- 
+ // stopwatch
+
+//  const display=document.getElementById("display");
+//  let timer=null;
+//  let stratTime=0;
+//  let elapseTime=0;
+//  let isRunning=false;
+
+
+//  function start(){
+//         if(!isRunning){
+//             stratTime=Date.now()-elapseTime;
+//             timer=setInterval(update,10);
+//             isRunning=true;
+
+//         }
+//  }
+//  function stop(){
+//     if(isRunning){
+//         clearInterval(timer);
+//         elapsetime=Date.now()-stratTime;
+//         isRunning=false;
+//     }
+    
+//  }
+//  function reset(){
+//      clearInterval(timer);
+//      stratTime=0;
+//      elapseTime=0;
+//      isRunning=false;
+//      display.textContent="00:00:00:00"
+//  }
+//  function update(){
+//     const currentTime=Date.now();
+//     elapseTime=currentTime-stratTime;
+//     let hours=Math.floor(elapseTime/(1000*60*60));
+//     let minutes=Math.floor(elapseTime/(1000*60)%60);
+//     let seconds=Math.floor(elapseTime/ 1000 % 60);
+//     let miniseconds=Math.floor(elapseTime % 1000 /10);
+
+//     hours=String(hours).padStart(2,"0");
+//     minutes=String(minutes).padStart(2,"0");
+//     seconds=String(seconds).padStart(2,"0");
+//     miniseconds=String(miniseconds).padStart(2,"0");
+//     display.textContent=`${hours}:${minutes}:${seconds}:${miniseconds}`;
+//  }
+
+//clock
+
+// const display=document.getElementById("display");
+
+// function updateClock(){
+//     let now =  new Date();
+//     let hours=now.getHours();
+//     let minutes=now.getMinutes();
+//     let seconds=now.getSeconds();
+
+//     hours=hours%12 || 12;
+//     let mid=hours >=12 ? "Am" : "PM";
+
+//     hours=String(hours).padStart(2,"0");
+//     minutes=String(minutes).padStart(2,"0");
+//     seconds=String( seconds).padStart(2,"0");
+
+//     display.textContent=`${hours}:${minutes}:${seconds}:${mid}`;
+
+// }
+// updateClock();
+// setInterval(updateClock,1000);
+
